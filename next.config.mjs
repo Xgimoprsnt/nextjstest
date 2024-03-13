5208cf165b15b2d3e27bef039f253c696c51a68e
X10 <x10@X10s-MacBook-Pro.local>
2024-03-13 15:33:56 +0700

    fix : path

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
-  // basePath: isProduction ? '/test' : '',
+  basePath: isProduction ? '/nextjstest' : '',
   assetPrefix: isProduction ? '/nextjstest' : '',
 };
 // dd
