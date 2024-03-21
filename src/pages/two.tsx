b7911de4c40614d11000a34e1dbeb3944277028d
xgimoprsnt <flukefitz@gmail.com>
2024-03-21 09:51:59 +0700

    fixxx word

--- src/pages/two.tsx
+++ src/pages/two.tsx
@@ -3,7 +3,7 @@ import LayoutTwo from '@/components/layout/layoutTwo'
 import type { NextPageWithLayout } from './_app'
  
 const Page: NextPageWithLayout = () => {
-  return <p>2222222333</p>
+  return <p>2222222333       page</p>
 }
  
 Page.getLayout = function getLayout(page: ReactElement) {
